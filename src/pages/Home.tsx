ec8970fed457c7b32c922a93c986edb5fc9a5cf8
Ubinquitous <ubinquitous1@gmail.com>
2022-11-16 02:52:57 +0900

    ADD : AllPost

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import AllPost from '../components/AllPost';
 import Header from '../components/Header';
+import Posting from '../components/Posting';
+import '../style/Home.scss';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home-wrap'>
             <Header />
+            <Posting />
+            <AllPost />
         </div>
     );
 };
